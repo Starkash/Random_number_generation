14da9038d4983a765dbe56681627db131e9b0981
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-31 03:15:58 +0530

    Add project files.

---
+++ Random_number_generation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Random_Num_gen
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Any num ");
+        int n = int.Parse(Console.ReadLine());
+        Random rnd = new Random();
+
+        for (int j = 1; j <= n; j++)
+        {
+            Console.WriteLine(rnd.Next(10, 50));
+            Console.WriteLine(rnd.Next(5));
+            Console.WriteLine(rnd.Next());
+
+
+        }
+
+
+    }
+}
